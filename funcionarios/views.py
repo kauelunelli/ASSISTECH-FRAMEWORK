72f2d43aa0ba932856df73698d51d9f83abf80aa
noscopejusthit <lunellikaue@gmail.com>
2021-09-10 01:20:10 -0300

    Acesso do Login funcionando

---
+++ funcionarios/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect 
+from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+from .models import *
+from .forms import OrderForm, CreateUserForm
+
+
+def registerPage(request):
+    form = UserCreationForm
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            messages.add_message(request, messages.SUCCESS, 'Usuário cadastrado com sucesso.')
+            form.save
+
+    context = {'form': form}
+
+    return render(request, 'accounts/register.html')
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('CPF')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return render(request, 'accounts/home.html')
+        else:
+            messages.info(request, 'Username Or password is incorrect ')
+    context = {}
+    return render(request, 'accounts/login.html')
+
+
+def home(request):
+    return render(request, 'accounts/home.html')
+
